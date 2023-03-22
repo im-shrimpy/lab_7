898dbb0d89fbdcebef914f0ef57c91fccd8f71cf
im-shrimpy <nadavmf1@gmail.com>
2023-03-22 16:10:27 -0400

    wrote stuff

---
+++ lab_7_nadavMitelman-Feit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define LEN 9
+
+void swap(int *a, int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void bubbleSort(*arr){
+    for(int i=0;i<LEN-1;i++)
+    {
+        int numSwaps=0;
+        for(int j=i;j<LEN-1;j++)
+        {
+            if(arr[j]>=arr[j+1])
+            {
+                swap(&arr[j],arr[j+1]);
+                numSwaps++;
+            }
+        }
+        printf("Iteration #%d: %d\n",i,num_swaps)
+    }
+}
+
+int main(void){
+    int array[LEN]= {97,16,45,63,13,22,7,58,72};
+    bubbleSort(array);
+    return 0;
+}
