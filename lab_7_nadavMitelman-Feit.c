eb1cc87775daaa6077e0232baa5547729aa36bb3
im-shrimpy <nadavmf1@gmail.com>
2023-03-22 20:29:38 -0400

    fixed bug

--- lab_7_nadavMitelman-Feit.c
+++ lab_7_nadavMitelman-Feit.c
@@ -2,23 +2,18 @@
 #include <stdio.h>
 #define LEN 9
 
-void swap(int *a, int *b)
-{
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-}
-
 void bubbleSort(int *arr)
 {
     for(int i=0;i<LEN-1;i++)
     {
         int numSwaps=0;
-        for(int j=i;j<LEN-i-1;j++)
+        for(int j=0;j<LEN-i-1;j++)
         {
             if(arr[j]>=arr[j+1])
             {
-                swap(&arr[j],&arr[j+1]);
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
                 numSwaps++;
             }
         }
@@ -29,5 +24,6 @@ void bubbleSort(int *arr)
 int main(void){
     int array[LEN]= {97,16,45,63,13,22,7,58,72};
     bubbleSort(array);
+    
     return 0;
 }
